a44f0cce2fbe10d46d909589d0e6aa3b7f1be117
Alkia <maxime@alkia.net>
2026-02-15 19:36:41 +0700

    header

--- __manifest__.py
+++ __manifest__.py
@@ -1,3 +1,23 @@
+################################################################################
+# Module:        Stock Input/Output Accounts Module for Odoo 19                   
+# Version:       19.0.2                              
+# Last Updated:  2026-01-04                          
+# Author:        M
+# Company:       Alkia IT Services Co., Ltd.
+# Location:      Khet Khlong Toei, Krung Thep Maha Nakhon, TH
+# Copyright (c): 2025-Now Alkia IT Services Co., Ltd.
+# License:       Proprietary (see LICENSE file)
+# Dependencies:  Python 3.12+, Odoo 19+              
+# All Rights Reserved.
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+# You should have received a copy of the License along with this program.
+# If not, refer to https://www.alkia.net/license
+#################################################################################
+
 {
     'name': 'Restore Stock Input/Output Accounts in v19',
     'version': '19.0.1.0.0',
