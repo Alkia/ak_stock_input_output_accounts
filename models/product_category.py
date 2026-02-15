2224f14273f279335a3c8d37ca2d70e4b4b524c5
Alkia <maxime@alkia.net>
2026-02-15 19:41:31 +0700

    go

--- models/product_category.py
+++ models/product_category.py
@@ -1,23 +1,3 @@
-################################################################################
-# Module:        Stock Input/Output Accounts Module for Odoo 19                   
-# Version:       19.0.2                              
-# Last Updated:  2026-01-04                          
-# Author:        M
-# Company:       Alkia IT Services Co., Ltd.
-# Location:      Khet Khlong Toei, Krung Thep Maha Nakhon, TH
-# Copyright (c): 2025-Now Alkia IT Services Co., Ltd.
-# License:       Proprietary (see LICENSE file)
-# Dependencies:  Python 3.12+, Odoo 19+              
-# All Rights Reserved.
-#
-#
-# This program is copyright property of the author mentioned above.
-# You can`t redistribute it and/or modify it.
-#
-# You should have received a copy of the License along with this program.
-# If not, refer to https://www.alkia.net/license
-#################################################################################
-
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
